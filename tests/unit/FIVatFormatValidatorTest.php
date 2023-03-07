36a3eed5503adaddb8b089508a7988e2566bba0e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-07 21:36:19 +0400

    [CVFV-4] completed test cases.

--- tests/unit/FIVatFormatValidatorTest.php
+++ tests/unit/FIVatFormatValidatorTest.php
@@ -30,8 +30,80 @@ class FIVatFormatValidatorTest extends TestCase
     {
         return [
             [
-                'vatNumber',
-                'isValid',
+                'vatNumber' => 'FI00000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'FI11111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'FI99999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'FI12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '00000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '11111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '99999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'FI123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'FI1234567',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1234567',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'Fi12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'fI12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'fi12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'DE12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '0',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '',
+                'isValid' => false,
             ],
         ];
     }
